086b28ff72841dc9df28a9ed25584ef3e924b13b
haritha1208 <harithasamsari1208@gmail.com>
2019-01-28 17:46:23 +0530

    second commit

---
+++ div.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int divf(int a,int b){
+int res ;
+return res = a / b;
+}
+int main(){
+int x,y;
+printf("Enter the numbers:");
+scanf("%d%d",&x,&y);
+printf("%d",mulf(x,y));
+return 0;
+}
---
+++ mul.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int mulf(int a,int b){
+int res;
+return res = a *b;
+}
+
+int main(){
+int x,y;
+printf("Enter the numbers:");
+scanf("%d%d",&x,&y);
+printf("%d",mulf(x,y));
+return 0;
+}
